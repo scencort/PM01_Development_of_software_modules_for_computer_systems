4ff6aea31d5956405e707dd6134f74812b8bc85c
Yaroslav <yaroslavpolyakov28352007@gmail.com>
2025-12-19 19:57:30 +0300

    ...

---
+++ Hotel/main.py
@@ -0,0 +1,14 @@
+from service import HotelService
+
+def main():
+    hs = HotelService()
+
+    hs.load_category("categories.txt")
+    hs.load_number("numbers.txt")
+    hs.load_citizen("citizens.txt")
+    hs.load_placement('placements.txt')
+
+    hs.save_free_number_json("free_number_json.json")
+
+if __name__ == "__main__":
+    main()
